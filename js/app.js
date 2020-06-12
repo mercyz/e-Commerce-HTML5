eb54e600d37111fdd250a0558dbf7dc1f84f3425
Uzor Mercy <mercy4hope@gmail.com>
2020-06-12 02:47:58 +0100

    Added Featured product column and banners

--- js/app.js
+++ js/app.js
@@ -6,3 +6,20 @@ $(".background_bg").each(function() {
         $(this).css('background-image', 'url(' + attr + ')');
     }
 });
+
+const productToggleLinks = document.querySelector(".nav-tabs")
+const productContainer = document.querySelectorAll('.tab-pan')
+let tabCounter = 0;
+productToggleLinks.addEventListener('click', function(e){
+    productContainer.forEach((item, indx) => {
+        if(indx == tabCounter){
+            item.classList.add('active_tab')
+            console.log(item)
+            return
+        }else{
+            item.classList.remove('active_tab');
+        }
+    })
+    tabCounter += 1;
+})
+
